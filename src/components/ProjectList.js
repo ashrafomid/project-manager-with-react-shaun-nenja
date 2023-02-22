a6a022c69047cd81a491af8da91ad10e713adf4c
Mohammad Ashraf <ashrafomid75@gmail.com>
2023-02-22 08:26:59 +0430

    final touches to project manager

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -10,7 +10,7 @@ export default function ProjectList({ projects }) {
   return (
     <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
-      {projects.map((project) => (
+      {projects && projects.map((project) => (
         <Link to={`/projects/${project.id}`} key={project.id}>
           <h4>{project.name}</h4>
           <p>Due by {project.dueDate.toDate().toDateString()}</p>
